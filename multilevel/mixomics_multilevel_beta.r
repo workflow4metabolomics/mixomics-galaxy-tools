e8e0e7f6db788e0d9b569bfb68bb53e26cfe615a
Guitton Yann <yann.guitton@gmail.com>
2017-02-06 17:40:39 +0100

    update with included withinVariation no needs of mixOmics

--- multilevel/mixomics_multilevel_beta.r
+++ multilevel/mixomics_multilevel_beta.r
@@ -26,18 +26,26 @@
 #             result (Robject) 
 ################################################################################
 
-#Redirect all stdout to the log file
-log_file=file("metams.log", open = "wt")
+# ----- LOG FILE -----
+log_file=file("multilevelout.log", open = "wt")
 sink(log_file)
 sink(log_file, type = "out")
 
+
+# ----- PACKAGE -----
+cat("\tPACKAGE INFO\n")
+
+pkgs=c("mixOmics","batch")
+for(pkg in pkgs) {
+  suppressPackageStartupMessages( stopifnot( library(pkg, quietly=TRUE, logical.return=TRUE, character.only=TRUE)))
+  cat(pkg,"\t",as.character(packageVersion(pkg)),"\n",sep="")
+}
+source_local <- function(fname){ argv <- commandArgs(trailingOnly = FALSE); base_dir <- dirname(substring(argv[grep("--file=", argv)], 8)); source(paste(base_dir, fname, sep="/")) }
+cat("\n\n");
+
 library(batch) #necessary for parseCommandArgs function
 
-source_local <- function(fname) {
-    argv <- commandArgs(trailingOnly = FALSE)
-    base_dir <- dirname(substring(argv[grep("--file=", argv)], 8))
-    source(paste(base_dir, fname, sep="/"))
-}
+
 print("first loadings OK")
 
 
@@ -63,7 +71,50 @@ flgC <- "\n"
 
 ## functions
 ##----------
-
+withinVariation<-function (X, design) 
+{
+    X = as.matrix(X)
+    rep.measures = factor(design[, 1])
+    factors = design[, -1, drop = FALSE]
+    if (any(summary(as.factor(rep.measures)) == 1)) 
+        stop("A multilevel analysis can not be performed when at least one some sample is not repeated.")
+    if ((ncol(factors) == 0) | (ncol(factors) == 1)) {
+        message("Splitting the variation for 1 level factor.")
+        indiv.names = rownames(X)
+        rownames(X) = as.character(rep.measures)
+        X.mean.indiv = matrix(apply(X, 2, tapply, rep.measures, 
+            mean, na.rm = TRUE), nrow = length(unique(rep.measures)), 
+            ncol = dim(X)[2], dimnames = list(levels(as.factor(rep.measures)), 
+                colnames(X)))
+        Xb = X.mean.indiv[as.character(rep.measures), ]
+        Xw = X - Xb
+        dimnames(Xw) = list(indiv.names, colnames(X))
+    }
+    else {
+        message("Splitting the variation for 2 level factors.")
+        Xm = colMeans(X)
+        Xs = apply(X, 2, tapply, rep.measures, mean, na.rm = TRUE)
+        Xs = Xs[rep.measures, ]
+        xbfact1 = apply(X, 2, tapply, paste0(rep.measures, factors[, 
+            1]), mean, na.rm = TRUE)
+        xbfact1 = xbfact1[paste0(rep.measures, factors[, 1]), 
+            ]
+        xbfact2 = apply(X, 2, tapply, paste0(rep.measures, factors[, 
+            2]), mean, na.rm = TRUE)
+        xbfact2 = xbfact2[paste0(rep.measures, factors[, 2]), 
+            ]
+        Xmfact1 = apply(X, 2, tapply, factors[, 1], mean, na.rm = TRUE)
+        Xmfact1 = Xmfact1[factors[, 1], ]
+        Xmfact2 = apply(X, 2, tapply, factors[, 2], mean, na.rm = TRUE)
+        Xmfact2 = Xmfact2[factors[, 2], ]
+        Xw = X + Xs - xbfact1 - xbfact2 + Xmfact1 + Xmfact2
+        Xw = sweep(Xw, 2, 2 * Xm)
+        dimnames(Xw) = dimnames(X)
+    }
+    return(invisible(Xw))
+}
+ ##end withinVariation to be removed after mixOmics package properly installed   
+    
 flgF <- function(tesC,
                  envC = topEnvC,
                  txtC = NA) { ## management of warning and error messages
@@ -88,11 +139,10 @@ flgF <- function(tesC,
 ## log file
 ##---------
 
-sink(argVc["information"])
+##sink(argVc["information"])
 
 cat("\nStart of the '", modNamC, "' Galaxy module call: ",
     format(Sys.time(), "%a %d %b %Y %X"), "\n", sep="")
-
 
 ## arguments
 ##----------
@@ -118,14 +168,14 @@ flgF("identical(rownames(xMN), rownames(samDF))", txtC = "Sample names (or numbe
 if (listArguments[["respL2"]]!="NULL"){
   cat("\n\nMultilevel (two levels)\n");
   tryCatch({
-    result <- withinVariation();
+    result <- withinVariation(xMN, design=samDF[,c(listArguments[["respL1"]],listArguments[["respL2"]])]);
   }, error = function(err) {
     stop(paste("There was an error when trying to run the Multilevel (two levels) function.\n\n",err));
   });
 } else {
     cat("\n\nMultilevel (one level)\n");
   tryCatch({
-    result <- withinVariation();
+    result<-withinVariation(xMN, design=samDF[,c(listArguments[["respL"]])]);
   }, error = function(err) {
     stop(paste("There was an error when trying to run the Multilevel (one level) function.\n\n",err));
   });
@@ -136,10 +186,8 @@ if (listArguments[["respL2"]]!="NULL"){
 if (exists("result")) {
   ## writing output files
   cat("\n\nWriting output files\n\n");
-  write.table(result$loadings$X, file=loadingVectorsX, sep="\t");
-  write.table(result$loadings$Y, file=loadingVectorsY, sep="\t");
-  write.table(result$variates$X, file=latentVariablesX, sep="\t");
-  write.table(result$variates$Y, file=latentVariablesY, sep="\t");
+  ##write.table(result, file=loadingVectorsX, sep="\t");
+ 
   tryCatch({
     save(result, file=resultfile);
   }, warning = function(w) {
